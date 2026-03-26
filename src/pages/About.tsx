b076a1086e902c5ddd10a410cc3bfaa7a456a77e
hokumcangus <mcangusohana2@gmail.com>
2026-03-26 13:13:29 -0700

    Initial commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,14 @@
+function About() {
+    return (
+            <div className="about">
+                <h1>About Me</h1>
+                <p>
+                    My name is Hoku McAngus, and I am a software developer with a passion for creating innovative and user-friendly applications.
+                    With a strong background in full-stack development, I have experience working with a variety of programming languages and frameworks.
+                    I am dedicated to continuous learning and growth in the field of software development, and I am excited to share my projects and experiences with you through this portfolio.
+                </p>
+            </div>
+    );
+}
+
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <div className ="home">
+            <h1>Aloha and welcome to my portfolio!</h1>
+            <p> My name is Hoku McAngus
+                and I am a software developer with a passion for creating innovative and user-friendly applications.
+                With a strong background in full-stack development, I have experience working with a variety of programming languages and frameworks.
+                I am dedicated to continuous learning and growth in the field of software development, and I am excited to share my projects and experiences with you through this portfolio.
+            </p>
+        </div>
+    )
+}
